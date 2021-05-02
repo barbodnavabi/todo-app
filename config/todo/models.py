6ab846bb2a42f9fa0364f8175ed24e0fd1f09141
barbodnavabi <barbodnvb@gmail.com>
2021-05-02 13:26:11 +0430

    todo

--- config/todo/models.py
+++ config/todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+class Todo(models.Model):
+    name=models.CharField(("نام شما"), max_length=300)
+    title=models.CharField(("عنوان"), max_length=300)
+    desceiption=models.TextField(("توضیحات"))
+    email=models.EmailField(("ایمیل"), max_length=254)
+    start_date = models.DateTimeField(("زمان شروع"), default=timezone.now)
+    end_date=  models.DateTimeField(("زمان پایان"),default=timezone.now)
+    
 
-# Create your models here.
+    class Meta:
+        verbose_name = ("Todo")
+        verbose_name_plural = ("Todos")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("todo_detail", kwargs={"pk": self.pk})
--- config/todo/views.py
+++ config/todo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from .models import Todo
 
 def index(request):
+    todo_list=Todo.objects.all()
     conext ={
+        'todo_list':todo_list
 
     }
     return render(request,'todo/index.html',conext)
